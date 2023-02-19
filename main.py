a1c1a02c989f0c575325b67caecf279f4f32b302
punitblack <93486178+punitblack@users.noreply.github.com>
2023-02-19 15:33:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import pyperclip
+import pyshorteners
+from tkinter import *
+
+root = Tk()
+root.geometry("400x200")
+root.title("URL Shortener")
+root.configure(bg="#49A")
+url = StringVar()
+url_addrerss = StringVar()
+
+def urlshortener():
+    urladdress = url.get()
+    url_short = pyshorteners.Shortener().tinyurl.short(urladdress)
+    url_addrerss.set(url_short)
+
+
+def copyurl():
+    url_short = url_addrerss.get()
+    pyperclip.copy(url_short)
+
+Label(root, text="MY URL Shortener", font="poppins").pack(pady=10)
+Entry(root, textvariable=url).pack(pady=5)
+Button(root, text="generate Short URL",command=urlshortener).pack(pady=7)
+Entry(root, textvariable=url_addrerss).pack(pady=5)
+Button(root,text="Copy URL", command = copyurl).pack(pady=5)
+
+root.mainloop()
